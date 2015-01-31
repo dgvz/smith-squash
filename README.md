05571621c6759b2ca316fa4dd45d40b1f1d7ac2f
Dylan Griffith <dyl.griffith@gmail.com>
2015-01-31 12:39:16 +1100

    doc additions and code improvements

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Smith::Squash
 
-TODO: Write a gem description
+Integrate squash into your smith agents.
 
 ## Installation
 
@@ -20,11 +20,59 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+This requires you to setup the following environment variables for your agents:
+* `SQUASH_URI`: The URI of your squash installation
+* `SQUASH_API_KEY`: The API key setup for your squash project
+
+If neither of these are set then squash just won't log, so that means you don't
+have to worry about any `if production` logic.
+
+Optionally you can set the follow environment variables:
+* `SQUASH_SKIP_SSL_VERIFICATION=true`: to skip ssl verification if your
+  certificate is suck
+* `REVISION_FILE_PATH`: if set squash reads this file to get the current
+  revision. You only need this if it's not a git repo
+
+If you want your agent to send messages to squash on an uncaught exception just
+setup your agent as follows:
+
+```ruby
+require 'smith/squash/squashable_agent'
+
+class MyAgent < Smith::Agent
+  include SquashableAgent
+
+  def run
+    enable_squash
+  end
+end
+```
+
+Or if you want to send specific errors to squash you can do:
+
+```
+require 'smith/squash/squashable_agent'
+
+class MyAgent < Smith::Agent
+  include SquashableAgent
+
+  def run
+  end
+
+  def do_something(arg)
+    df = do_deferred
+
+    df.errback do |e|
+      squash_notify(e, {:arg => arg})
+    end
+  end
+end
+```
+
 
 ## Contributing
 
-1. Fork it ( https://github.com/[my-github-username]/smith-squash/fork )
+1. Fork it ( https://github.com/dgvz/smith-squash/fork )
 2. Create your feature branch (`git checkout -b my-new-feature`)
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
--- lib/smith/squash/squashable_agent.rb
+++ lib/smith/squash/squashable_agent.rb
@@ -1,31 +1,60 @@
 require 'squash/ruby'
 
 module SquashableAgent
+
+  # Enable squash logging of any uncaught exception that results in the agent
+  # dying.
+  def enable_squash
+    if use_squash?
+      Squash::Ruby.configure(squash_config_options)
+
+      on_exception do |exception|
+        Squash::Ruby.notify(exception)
+      end
+    end
+  end
+
+  # Send an aribtrary exception to squash.
+  #
+  # @param error [Exception] the exception to notify squash about
+  # @param additional_data [Hash] some additional meta data to send with the
+  #   exception
+  def squash_notify(error, additional_data = {})
+    if use_squash?
+      Squash::Ruby.notify(exception)
+    end
+  end
+
+  private
+
   def use_squash?
     squash_host && squash_api_key
   end
 
   def squash_host
-    ENV["SQUASH_HOST"]
+    ENV["SQUASH_URI"]
   end
 
   def squash_api_key
     ENV["SQUASH_API_KEY"]
   end
 
-  def enable_squash
-    if use_squash?
-      Squash::Ruby.configure(
-        :api_host => squash_host,
-        :api_key => squash_api_key,
-        :environment => Smith.environment.to_s,
-        :revision_file => Pathname.new("~/app/current/REVISION").expand_path.to_s,
-        :skip_ssl_verification => true
-      )
+  def skip_ssl_verification?
+    ENV["SQUASH_SKIP_SSL_VERIFICATION"].to_s == "true"
+  end
 
-      on_exception do |exception|
-        Squash::Ruby.notify(exception)
-      end
-    end
+  def revision_file_path
+    ENV["REVISION_FILE_PATH"]
+  end
+
+  def squash_config_options
+    options = {
+      :api_host => squash_host,
+      :api_key => squash_api_key,
+      :environment => Smith.environment.to_s,
+      :skip_ssl_verification => skip_ssl_verification?
+    }
+    options.merge!(:revision_file_path => revision_file_path) if revision_file_path
+    options
   end
 end
